1af47b28f6209daab255949fdf080a3fcf958d86
Mahabub <mahabub775@gmail.com>
2020-02-29 19:45:20 +0600

    change

--- contact_form.php
+++ contact_form.php
@@ -1,21 +1,8 @@
-<?php
-$name = $_POST['name'];
-$Visitor_Email = $_POST['email'];
-$Visitior_phone = $_POST['phone'];
-$Message = $_POST['message'];
+<html>
+<body>
 
-$email_form = 'mahabub12774@gmail.com';
-$email_subject = 'New Form Submission';
-$email_body = "User Name : $name .\n".
-					"User Email : $Visitor_Email.\n".
-					"User Phone : $Visitior_phone.\n".
-					"User Message :$Message.\n";
-					
-					
-	$to = "mahabub775@gmail.com";
-	$headers = "From : $email_form \r\n";
-	$headers .="Replay -To : $Visitor_Email \r\n";
-	
-	mail($to, $email_subject,$email_body,$headers);
-	header("Location : index.html");
-?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
